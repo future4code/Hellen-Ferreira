0f20c95c1282857fc9fe3b4f9e20f244935b4cbe
Hellen Ferreira <contatohellenmarina@gmail.com>
2021-10-28 22:33:55 -0300

    astromatch-pront

---
+++ projeto-astromatch/astromatch/src/styled.js
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #eeeeee;
+  text-align: center;
+`;
+
+export const Card = styled.div`
+  width: 28vw;
+  height: 83vh;
+  border-radius: 15px;
+  box-shadow: 2px 2px 8px #595858;
+  text-align: center;
+`;
+export const CardText = styled.div`
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  height: 7%;
+  display: flex;
+`;
+
+export const CardBio = styled.div`
+  width: 70%;
+
+  display: flex;
+  margin-left: 60px;
+  font-size: 14px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+export const ImagemCard = styled.img`
+  margin-top: 10px;
+  border-radius: 80%;
+  width: 70%;
+  height: 50%;
+  box-shadow: 2px 2px 8px #595858;
+`;
+
+export const ImagemButton = styled.button`
+  background-color: transparent;
+  border: none;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-top: 10px;
+`;
+
+export const ImagemB = styled.img`
+  width: 45px;
+`;
+
+export const ImagemLogo = styled.img`
+  width: 8vw;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ButtonHeader = styled.button`
+  margin-left: 120px;
+  background-color: transparent;
+  border: none;
+`;
+
+export const ImagemB2 = styled.img`
+  width: 3vw;
+  margin-left: 100px;
+  border: none;
+`;
+
+export const Separador = styled.img`
+  width: 30vw;
+`;
+
+export const CardPage2 = styled.div`
+  width: 100%;
+  height: 8vh;
+  display: flex;
+  margin-bottom: 5px;
+  margin-left: 10px;
+  flex-direction: row;
+  margin-top: 5px;
+  text-align: center;
+`;
+
+export const ImageCardMatch = styled.img`
+  width: 60px;
+  min-height: 60px;
+  border-radius: 80%;
+  margin-right: 10px;
+  box-shadow: 2px 2px 8px #595858;
+`;
+
+export const CardMatch = styled.div`
+  width: 28vw;
+  padding-bottom: 10px;
+  min-height: 83vh;
+  border-radius: 15px;
+  box-shadow: 2px 2px 8px #595858;
+  text-align: center;
+`;
